1e8d753fcb5d348acb5eb8ea113e7fc150137016
cuda@jupyter <cuda@jupyter.arz.oeaw.ac.at>
2024-08-28 10:06:41 -0400

    rework test inference

--- Dockerfile
+++ Dockerfile
@@ -4,3 +4,5 @@ RUN pip install fasttext==0.9.2
 RUN pip install gensim==4.3.2
 RUN pip install PyYAML==6.0.1
 RUN pip install ipdb==0.13.13 
+WORKDIR /veld/code/
+
--- src/jupyter_notebook/test.py
+++ src/jupyter_notebook/test.py
@@ -4,7 +4,5 @@ import fasttext
 model_path = "/veld/data/veld_data_8_fasttext_models/data/test/test.bin"
 model = fasttext.load_model(model_path)
 
-word = "Europa"
-word_vector = model.get_word_vector(word)
+print(model.get_word_vector("und"))
 
-print(word_vector)
